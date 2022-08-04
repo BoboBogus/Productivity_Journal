9a33bd2e79e050fbbdc6c7d544a57e27590c26d1
BoboBogus <75058008+BoboBogus@users.noreply.github.com>
2022-08-04 00:16:58 -0400

    Basic FileDataHandling with the txt files

--- src/utils/DataHandler.java
+++ src/utils/DataHandler.java
@@ -1,60 +1,103 @@
 package utils;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class DataHandler {
-    static int GoalComponents = 2;
-    static int ReflectionComponents = 3;
 
     // Create New Goal in the txt file
     public static void CreateNewGoal(GoalContainer container) throws IOException {
         File file = new File("data/Goal_Data.txt");
         FileWriter fileWriter = new FileWriter(file, true);
         fileWriter.write(container.Date + "\n");
-        fileWriter.write(container.Goal + "\n");
-        fileWriter.write(container.Difficulty + "\n");
+        fileWriter.write("Goal =" + container.Goal + "\n");
+        fileWriter.write("Goal =" + container.Difficulty + "\n");
     }
 
     public static void CreateNewReflection(ReflectionContainer container) throws IOException {
         File file = new File("data/Goal_Data.txt");
         FileWriter fileWriter = new FileWriter(file, true);
         fileWriter.write(container.Date + "\n");
-        fileWriter.write(container.Goal + "\n");
-        fileWriter.write(container.Description + "\n");
-        fileWriter.write(container.stars + "\n");
-        fileWriter.write(container.points + "\n");
+        fileWriter.write("Goal =" + container.Goal + "\n");
+        fileWriter.write("Description =" + container.Description + "\n");
+        fileWriter.write("stars =" + container.stars + "\n");
+        fileWriter.write("difficulty =" + container.difficulty + "\n");
     }
 
-    public static void readElements(int lineStart) {
-
-    }
-
-    public static String getValue(String data, int index) {
-        int chosenIndex = 0;
-        int followUp = data.length();
-        int indexCheck = 0;
-        boolean first = false;
-        // fix the :recognition
-        // if zero get (0):0:0:0
-
-        if (index == -1) {
-            chosenIndex = 0;
-            first = false;
-        }
-        for (int i = 0; i < data.length(); i++) {
-            if (data.charAt(i) == '=') {
-                if (indexCheck == index && !first) {
-                    chosenIndex = i + 1;
+    // outputs a GoalContainer based on the starting position of the text container
+    // in the txt file
+    public static GoalContainer readGoalElements(int lineStart) {
+        String Date = "";
+        String Goal = "";
+        int Difficulty = -1;
+        try {
+            File data = new File("DATA/Goal_Data.txt");
+            Scanner dataRead = new Scanner(data);
+            for (int lineNum = 1; dataRead.hasNext(); lineNum++) {
+                if (lineNum == lineStart) {
+                    Date = getValue(dataRead.nextLine());
                 }
-                if (indexCheck == index + 1) {
-                    followUp = i;
+                if (lineNum == lineStart + 1) {
+                    Goal = getValue(dataRead.nextLine());
                 }
-                indexCheck++;
+                if (lineNum == lineStart + 2) {
+                    Difficulty = Integer.parseInt(getValue(dataRead.nextLine()));
+                }
             }
+            dataRead.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
         }
+        GoalContainer container = new GoalContainer(Date, Goal, Difficulty);
+        return container;
+    }
+
+    // outputs a ReflectionContainer based on the starting position of the text
+    // container in the txt file
+    public static ReflectionContainer readRelectionElements(int lineStart) {
+        String Date = "";
+        String Goal = "";
+        String Description = "";
+        int Stars = -1;
+        int Difficulty = -1;
+        try {
+            File data = new File("DATA/Goal_Reflection_Data.txt");
+            Scanner dataRead = new Scanner(data);
+            for (int lineNum = 1; dataRead.hasNext(); lineNum++) {
+                if (lineNum == lineStart) {
+                    Date = getValue(dataRead.nextLine());
+                }
+                if (lineNum == lineStart + 1) {
+                    Goal = getValue(dataRead.nextLine());
+                }
+                if (lineNum == lineStart + 2) {
+                    Description = getValue(dataRead.nextLine());
+                }
+                if (lineNum == lineStart + 3) {
+                    Stars = Integer.parseInt(getValue(dataRead.nextLine()));
+                }
+                if (lineNum == lineStart + 4) {
+                    Difficulty = Integer.parseInt(getValue(dataRead.nextLine()));
+                }
+            }
+            dataRead.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        ReflectionContainer container = new ReflectionContainer(Date, Goal, Description, Stars, Difficulty);
+        return container;
+    }
+
+    public static String getValue(String data) {
         String result = "";
+        for (int i = data.indexOf("=") + 2; i < data.length(); i++) {
+            result += data.charAt(i) + "";
+        }
         return result;
     }
 }
--- src/utils/ReflectionContainer.java
+++ src/utils/ReflectionContainer.java
@@ -5,13 +5,17 @@ public class ReflectionContainer {
     String Goal;
     String Description;
     int stars;
+    int difficulty;
     int points;
 
-    public ReflectionContainer(String Date, String Goal, String Description, int stars, int points) {
+    public ReflectionContainer(String Date, String Goal, String Description, int stars, int difficulty) {
         this.Date = Date;
         this.Goal = Goal;
         this.Description = Description;
         this.stars = stars;
-        this.points = points;
+        this.difficulty = difficulty;
+
+        // difficulty x stars conversion rate
+        points = stars * difficulty;
     }
 }
